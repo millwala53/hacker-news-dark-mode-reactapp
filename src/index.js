800f414b71d5b4d1233e63d4e622f0a82b3dc12e
Murtaza Millwala <millwala5@gmail.com>
2019-10-16 13:04:21 +0500

    initalized redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import GlobalStyles from './styles/globals';
-ReactDOM.render(<App />, document.getElementById('root'));
-GlobalStyles()
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import GlobalStyles from "./styles/globals";
+import { Provider } from "react-redux";
+import configureStore from "store";
+
+GlobalStyles();
+
+const renderApp = () => {
+  const initialState = {};
+  const store = configureStore(initialState);
+  store.dispatch({ type: "@hnClone/@@INIT" });
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+renderApp();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
